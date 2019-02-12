99dfe00c88d7652398a77d3e9da1594dfad7eb46
jchraibi <jchraibi@redhat.com>
2019-02-12 16:32:14 +0100

    modified template orange-nodejs-mongodb

--- watch_template.sh
+++ watch_template.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "----------------------------------------------------------------" >> /tmp/template.txt
 echo "Applying modifications for template $2 " >> /tmp/template.txt
+oc get template $2 -o yaml > orange.yaml
+git add .
+git commit -m "modified template $2"
+git push -u origin master
 oc process $2 | oc apply -f - >> /tmp/template.txt
 echo "Finished applying modifications" >> /tmp/template.txt
 echo "----------------------------------------------------------------" >> /tmp/template.txt
